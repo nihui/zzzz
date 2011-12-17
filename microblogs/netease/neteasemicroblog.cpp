d95001872c0d5b7c52c4aa749e8fb0fff0f07e0b
Ni Hui <shuizhuyuanluo@126.com>
2011-12-17 17:18:01 +0800

    urlize post content in netease plugin

--- microblogs/netease/neteasemicroblog.cpp
+++ microblogs/netease/neteasemicroblog.cpp
@@ -1,6 +1,7 @@
 #include "neteasemicroblog.h"
 
 #include <types.h>
+#include <utility.h>
 
 #include <QDebug>
 #include <QUrl>
@@ -125,6 +126,7 @@ void NeteaseMicroBlog::readPostFromJsonMap( const QVariantMap& varmap, Zzzz::Pos
     QVariantMap usermap = varmap["user"].toMap();
     readUserFromJsonMap( usermap, post.user );
     post.text = varmap["text"].toString();
+    Zzzz::Utility::urlize( post.text );
     post.creationDateTime = varmap["created_at"].toString();
     post.replyToStatusId = varmap["in_reply_to_status_id"].toString();
     post.replyToUserId = varmap["in_reply_to_user_id"].toString();
